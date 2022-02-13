da486e6af77e4a32b5f98c1c78d99618e54e3477
Julian Toscani <julian_toscani@gmx.de>
2022-02-13 21:10:50 +0100

    use units relative to size

--- worklets/headerHighlight.js
+++ worklets/headerHighlight.js
@@ -1,22 +1,27 @@
-registerPaint(
-  "headerHighlight",
-  class {
-    /*
+class HeaderHighlight {
+  /*
          define if alphatransparency  is allowed alpha
          is set to true by default. If set to false, all
          colors used on the canvas will be fully opaque
       */
-    static get contextOptions() {
-      return { alpha: true };
-    }
+  static get contextOptions() {
+    return { alpha: true };
+  }
 
-    /*
+  /*
           ctx is the 2D drawing context
           a subset of the HTML5 Canvas API.
       */
-    paint(ctx) {
-      ctx.fillStyle = "hsla(55, 90%, 60%, 1.0)";
-      ctx.fillRect(0, 15, 200, 20); /* order: x, y, w, h */
-    }
+  paint(ctx, size) {
+    console.log(size);
+    ctx.fillStyle = "hsla(55, 90%, 60%, 1.0)";
+    ctx.fillRect(
+      0,
+      size.height / 2,
+      size.width * 0.8,
+      size.height / 2
+    ); /* order: x, y, w, h */
   }
-);
+}
+
+registerPaint("headerHighlight", HeaderHighlight);

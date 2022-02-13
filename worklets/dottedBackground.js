a61c8a72ca6e7f2a27e44b6b14372f04285f02e4
Julian Toscani <julian_toscani@gmx.de>
2022-02-13 22:58:25 +0100

    use circle in background

--- worklets/dottedBackground.js
+++ worklets/dottedBackground.js
@@ -10,7 +10,7 @@ class DottedBackgound {
   paint(ctx, size, props) {
     const dotSize = parseCssVariable(props, "--dot-size", 2);
     const dotSpacing = parseCssVariable(props, "--dot-spacing", 20);
-    const dotPadding = parseCssVariable(props, "--dot-size", dotSpacing / 2);
+    const dotPadding = parseCssVariable(props, "--dot-padding", dotSpacing / 2);
 
     const columns = Math.floor(size.width / dotSpacing);
     const rows = Math.floor(size.width / dotSpacing);
@@ -18,12 +18,12 @@ class DottedBackgound {
     ctx.fillStyle = props.get("--dot-color")[0] ?? "black";
     for (let colCount = 0; colCount < columns; colCount++) {
       for (let rowCount = 0; rowCount < rows; rowCount++) {
-        ctx.fillRect(
-          dotPadding + colCount * dotSpacing,
-          dotPadding + rowCount * dotSpacing,
-          dotSize,
-          dotSize
-        );
+        const centerX = dotPadding + colCount * dotSpacing;
+        const centerY = dotPadding + rowCount * dotSpacing;
+
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, dotSize, 0, 2 * Math.PI, false);
+        ctx.fill();
       }
     }
   }
